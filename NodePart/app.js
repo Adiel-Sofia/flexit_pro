3ffd3487e3577e04e08b6b05efad068962a08862
Adiel-Sofia <adiel13150@gmail.com>
2025-05-19 13:03:41 +0300

    second commit

--- NodePart/app.js
+++ NodePart/app.js
@@ -7,13 +7,13 @@ app.use("/uploads", express.static("uploads")); // Distributing uploaded files
 app.use(express.json());
 
 app.use("/user", userRoutes);
-// app.use((err, req, res, next) => {
-//   console.error(err); // Log error
-//   res.status(500).json({
-//     error: "Internal Server Error",
-//     message: err.message,
-//   });
-// });
+app.use((err, req, res, next) => {
+  console.error(err); // Log error
+  res.status(500).json({
+    error: "Internal Server Error",
+    message: err.message,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
